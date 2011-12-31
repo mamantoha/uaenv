fb1a506414f493ff0d976c57ca0578289ce64ec2
Anton Maminov <anton.linux@gmail.com>
2011-12-31 19:52:59 +0200

    fix test for transliteration

--- test/test_transliteration.rb
+++ test/test_transliteration.rb
@@ -7,11 +7,11 @@ class TransliterationTestCase < Test::Unit::TestCase
   def test_national_transliteration
     assert_equal "Zghurskyi", "Згурський".translify
     assert_equal "Galagan", "Ґалаґан".translify
-    assert_equal "Ternopil", "Тернопіль".translify_natinal
+    assert_equal "Ternopil", "Тернопіль".translify_national
     assert_equal "Yizhakevych", "Їжакевич".translify_national
   end
 
-  def test_national_transliteration
+  def test_iso_transliteration
     assert_equal "Zorâna", "Зоряна".translify_iso9a
     assert_equal "Ģalaģan", "Ґалаґан".translify_iso9a
     assert_equal "Yizhakevy`ch", "Їжакевич".translify_iso9b
